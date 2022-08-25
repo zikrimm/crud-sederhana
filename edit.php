21e315d83ca56f3928812d58ca89c4d4992998d1
zikrimm <79511833+zikrimm@users.noreply.github.com>
2022-08-25 17:55:56 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -27,42 +27,43 @@ if( isset($_POST["submit"]) ) {
 
 }
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Ubah data pegawai</title>
-</head>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Ubah Data Pegawai</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
 <body>
-	<h1>Ubah data pegawai</h1>
+    <div class="container mt-4">
+  <h1>Ubah data pegawai</h1>
 
-	<form action="" method="post">
-		<input type="hidden" name="id" value="<?= $pegawai["id"]; ?>">
-		<ul>
-			<li>
-				<label for="nik">NIK : </label>
-				<input type="text" name="nik" id="nik" required value="<?= $pegawai["nik"]; ?>">
-			</li>
-			<li>
-				<label for="nama">Nama : </label>
-				<input type="text" name="nama" id="nama" value="<?= $pegawai["nama"]; ?>">
-			</li>
-			<li>
-				<label for="email">Email :</label>
-				<input type="text" name="email" id="email" value="<?= $pegawai["email"]; ?>">
-			</li>
-			<li>
-				<label for="status">status :</label>
-				<input type="text" name="status" id="status" value="<?= $pegawai["status"]; ?>">
-			</li>
-			<li>
-				<button type="submit" name="submit">Ubah Data!</button>
-			</li>
-		</ul>
+<form action="" method="post">
+    <input type="hidden" name="id" value="<?= $pegawai["id"]; ?>">
+    <div class="mb-3">
+        <label for="nik" class="form-label">NIK</label>
+        <input type="text" class="form-control" name="nik" id="nik" value="<?= $pegawai["nik"]; ?>">
+    </div>
+    <div class="mb-3">
+        <label for="nama" class="form-label">Nama</label>
+        <input type="text" class="form-control" name="nama" id="nama" value="<?= $pegawai["nama"]; ?>" >
+    </div>
+    <div class="mb-3">
+        <label for="email" class="form-label">Email</label>
+        <input type="email" class="form-control" name="email" id="email" value="<?= $pegawai["email"]; ?>" >
+    </div>
+    <div class="mb-3">
+        <label for="status" class="form-label">Status</label>
+        <input type="text" class="form-control" name="status" id="status" value="<?= $pegawai["status"]; ?>" >
+    </div>
 
-	</form>
+    <button class="btn btn-primary" name="submit">Ubah Data</button>
+    
 
-
-
-
-</body>
+</form>
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
 </html>
+
